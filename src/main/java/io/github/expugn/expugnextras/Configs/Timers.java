292ee52d75eeb539196c0a213a2c0eb0112fee4f
Expugn <calvinye@Calvins-MacBook-Pro.local>
2016-04-23 18:07:54 -0700

    v4.0 (SNAPSHOT) - Plugin Cleanup: Timers
    “Its time.”
    
    - DungeonTimers
      > Moved config specific functions to the config class.
      > Removed ChatColor
    - v4.0 Progress
      > Working Features:
        * ExpugnCommand
        * ItemDrop
        * Warps
        * Timers
      > WIP Features
        * Marriage
        * ListTitles
        * TimeTrials

--- src/main/java/io/github/expugn/expugnextras/Configs/Timers.java
+++ src/main/java/io/github/expugn/expugnextras/Configs/Timers.java
@@ -1,5 +1,8 @@
 package io.github.expugn.expugnextras.Configs;
 
+import java.util.Set;
+import java.util.UUID;
+
 import io.github.expugn.expugnextras.ExpugnExtras;
 
 /**
@@ -20,5 +23,77 @@ public class Timers extends ConfigurationFile
 	public Timers(ExpugnExtras plugin) 
 	{
 		super(plugin, "timers");
+	}
+
+	public void createDungeon(String dungeon_name, long time)
+	{
+		set("dungeons." + dungeon_name + ".cooldown", time);
+		set("dungeons." + dungeon_name + ".players.key", null);
+	}
+	
+	public void deleteDungeon(String dungeon_name)
+	{
+		set("dungeons." + dungeon_name, null);
+	}
+	
+	public void changeDungeonTime(String dungeon_name, long time)
+	{
+		set("dungeons." + dungeon_name + ".cooldown", time);
+	}
+	
+	public void setPlayerTime(String dungeon_name, UUID playerUUID, Object data)
+	{
+		set("dungeons." + dungeon_name + ".players." + playerUUID, data);
+	}
+	
+	public long getPlayerTime(String dungeon_name, UUID playerUUID)
+	{
+		return getLong("dungeons." + dungeon_name + ".players." + playerUUID);
+	}
+	
+	public long getPlayerTimeLeft(String dungeon_name, UUID playerUUID)
+	{
+		return getPlayerTime(dungeon_name, playerUUID) - System.currentTimeMillis();
+	}
+	
+	//-----------------------------------------------------------------------
+	/**
+	 * Checks if the supplied dungeon is supported.
+	 * 
+	 * @param name  Player who sent the command
+	 * @return  true if the dungeon exists in the file, else false.
+	 */
+	public boolean checkDungeon(String dungeon_name) 
+	{
+		if (getDungeonList().contains(dungeon_name))
+			return true;
+		return false;
+	}
+	
+	//-----------------------------------------------------------------------
+	/**
+	 * Removes expired entries in the config file.
+	 */
+	public void cleanConfig()
+	{
+		for (String dungeon_name : getDungeonList()) 
+		{
+			for (String key : getConfigurationSectionKeys("dungeons." + dungeon_name + ".players"))
+			{
+				if (System.currentTimeMillis() > getLong("dungeons." + dungeon_name + ".players." + key))
+					set("dungeons." + dungeon_name + ".players." + key, null);
+			}
+		}
+	}
+	
+	//-----------------------------------------------------------------------
+	/**
+	 * Returns all list of all dungeons created.
+	 * 
+	 * @return  A set of all entries under 'dungeons' in the config
+	 */
+	public Set<String> getDungeonList()
+	{
+		return getConfigurationSectionKeys("dungeons");
 	}
 }
